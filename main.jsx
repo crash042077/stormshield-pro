a82b74257fe7b26ba1e63d7c8a3f5fdcc8c37cbf
crash042077 <j.smith070719@gmail.com>
2026-01-08 09:44:48 -0600

    Update6644 main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// This tells the app the 'Brain' is still inside the src folder
 import App from './src/App.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
