1d85c9d1a411c32b16976beb8869c91441fbdf45
crash042077 <j.smith070719@gmail.com>
2026-01-07 22:57:55 -0600

    Update2 main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // This keeps your styling working!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
